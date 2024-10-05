745a57bb7a763e589d1ce0d2087b6a6cbbe05b40
sk1fO <stamlerigo@gmail.com>
2024-10-05 15:31:11 +0700

    односвязный список ГОТОВО

---
+++ Lab1Sem3/linkedlist.h
@@ -0,0 +1,115 @@
+#ifndef linkedlist
+#define linkedlist
+
+#include <iostream>
+
+using namespace std;
+
+struct Node {
+    string val;
+    Node* next;
+    Node(string _val) : val(_val), next(nullptr){}
+};
+
+struct list{
+    Node* first;
+    Node* last;
+
+    list() : first(nullptr), last(nullptr){}
+
+    bool isEmpty(){
+        return first == nullptr;
+    }
+
+
+    void push_back(string _val){
+        Node* p = new Node (_val);
+        if(isEmpty()) {
+            first = p;
+            last = p;
+            return;
+        }
+        last -> next = p;
+        last = p;
+    }
+
+    void push_first(string _val){
+        Node* p = new Node (_val);
+        if(isEmpty()) {
+            first = p;
+            last = p;
+            return;
+        }
+
+        p->next = first;
+        first = p;
+    }
+
+    void print(){
+        if (isEmpty()) return;
+
+        Node* p = first;
+        while (p){
+            cout << p -> val << " ";
+            p = p->next;
+        }
+        cout << endl;
+    }
+    
+    Node* find(string _val) {
+        Node* p = first;
+        while (p && p->val != _val) p = p->next;
+        return (p && p->val == _val) ? p : nullptr;
+    }
+
+    void remove_first() {
+        if (isEmpty()) return;
+        Node* p = first;
+        first = p->next;
+        delete p;
+    }
+
+    void remove_last() {
+        if (isEmpty()) return;
+        if (first == last) {
+            remove_first();
+            return;
+        }
+        Node* p = first;
+        while (p->next != last) p = p->next;
+        p->next = nullptr;
+        delete last;
+        last = p;
+    }
+
+    void remove(string _val) {
+        if (isEmpty()) return;
+        if (first->val == _val) {
+            remove_first();
+            return;
+        }
+        else if (last->val == _val) {
+            remove_last();
+            return;
+        }
+        Node* slow = first;
+        Node* fast = first->next;
+        while (fast && fast->val != _val) {
+            fast = fast->next;
+            slow = slow->next;
+        }
+        if (!fast) {
+            cout << "This element does not exist" << endl;
+            return;
+        }
+        slow->next = fast->next;
+        delete fast;
+    }
+
+
+
+};
+
+
+#endif
+
--- Lab1Sem3/main.cpp
+++ Lab1Sem3/main.cpp
@@ -1,52 +1,21 @@
 #include <iostream>
+#include "linkedlist.h"
 using namespace std;
 
-struct Node {
-    string val;
-    Node* next;
-    Node(string _val) : val(_val), next(nullptr){}
-};
 
-struct list{
-    Node* first;
-    Node* last;
-
-    list() : first(nullptr), last(nullptr){}
-
-    bool isEmpty(){
-        return first == nullptr;
-    }
-
-
-    void push_back(string _val){
-        Node* p = new Node (_val);
-        if(isEmpty()) {
-            first = p;
-            last = p;
-            return;
-        }
-        last -> next = p;
-        last = p;
-    }
-
-    void print(){
-        if (isEmpty()) return;
-
-        Node* p = first;
-        while (p){
-            cout << p -> val << " ";
-            p = p->next;
-        }
-        cout << endl;
-    }
-
-};
 
 int main(){
     list flist;
     flist.push_back("Lol");
     flist.push_back("123");
+    flist.push_first("test");
+    flist.push_first("test2");
+    flist.remove_first();
+    flist.remove_last();
+    flist.remove("Lol");
+    cout << flist.find("test") << endl;
     flist.print();
+
 
     return 0;
 }
