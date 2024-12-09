dc410308b97a6d4253da0dcb24fe8a5fd6ef80ad
sk1fO <stamlerigo@gmail.com>
2024-12-09 09:57:13 +0700

    lab4_3

--- Lab4Sem3/3.cpp
+++ Lab4Sem3/3.cpp
@@ -1,207 +1,198 @@
 #include <iostream>
-#include <vector>
 #include <thread>
+#include <vector>
 #include <mutex>
-#include <condition_variable>
-#include <random>
-#include <chrono>
+#define MAX 20
+using namespace std;
 
-// Глобальные переменные
-int NUM_PROCESSES;
-int NUM_RESOURCES;
-std::vector<int> available;
-std::vector<std::vector<int>> max;
-std::vector<std::vector<int>> allocation;
-std::vector<std::vector<int>> need;
+class Bankers
+{
+private:
+    int al[MAX][MAX], m[MAX][MAX], n[MAX][MAX], avail[MAX]; // Матрицы текущих, максимума, нужды и доступные ресурсы
+    int nop, nor, k, result[MAX], pnum, work[MAX], finish[MAX]; // Число процессов, ресурсов, индекс результата, результат, номер процесса, рабочий массив, завершенные процессы
+    mutex mtx; // Мьютекс для синхронизации доступа к общим ресурсам
 
-std::mutex mtx;
-std::condition_variable cv;
-bool safe_state = true;
+public:
+    Bankers(); // Конструктор класса
+    void input(); // Ввод данных
+    void method(); // Основной метод алгоритма банкира
+    int search(int); // Поиск подходящего процесса
+    void display(); // Вывод результатов
+    void process(int i); // Обработка процесса в отдельном потоке
+};
 
-// Функция для инициализации данных
-void initialize() {
-    // Запрос количества процессов и ресурсов
-    std::cout << "Введите количество процессов: ";
-    std::cin >> NUM_PROCESSES;
-    std::cout << "Введите количество ресурсов: ";
-    std::cin >> NUM_RESOURCES;
-
-    // Инициализация векторов
-    available.resize(NUM_RESOURCES, 0);
-    max.resize(NUM_PROCESSES, std::vector<int>(NUM_RESOURCES, 0));
-    allocation.resize(NUM_PROCESSES, std::vector<int>(NUM_RESOURCES, 0));
-    need.resize(NUM_PROCESSES, std::vector<int>(NUM_RESOURCES, 0));
-
-    // Автозаполнение случайными данными
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_int_distribution<> dis(1, 10);
-
-    // Заполнение доступных ресурсов
-    for (int i = 0; i < NUM_RESOURCES; ++i) {
-        available[i] = dis(gen);
-    }
-
-    // Заполнение максимальных требований
-    for (int i = 0; i < NUM_PROCESSES; ++i) {
-        for (int j = 0; j < NUM_RESOURCES; ++j) {
-            max[i][j] = dis(gen);
+Bankers::Bankers()
+{
+    k = 0;
+    for (int i = 0; i < MAX; i++)
+    {
+        for (int j = 0; j < MAX; j++)
+        {
+            al[i][j] = 0;
+            m[i][j] = 0;
+            n[i][j] = 0;
         }
+        avail[i] = 0;
+        result[i] = 0;
+        finish[i] = 0;
     }
-
-    // Заполнение текущих выделенных ресурсов
-    for (int i = 0; i < NUM_PROCESSES; ++i) {
-        for (int j = 0; j < NUM_RESOURCES; ++j) {
-            allocation[i][j] = dis(gen) % (max[i][j] + 1);
-        }
-    }
-
-    // Вычисление нужных ресурсов
-    for (int i = 0; i < NUM_PROCESSES; ++i) {
-        for (int j = 0; j < NUM_RESOURCES; ++j) {
-            need[i][j] = max[i][j] - allocation[i][j];
-        }
-    }
-
-    // Вывод инициализированных данных
-    std::cout << "Доступно ресурсов: ";
-    for (int i = 0; i < NUM_RESOURCES; ++i) {
-        std::cout << available[i] << " ";
-    }
-    std::cout << std::endl;
-
-    std::cout << "Максимально ресурсов: " << std::endl;
-    for (int i = 0; i < NUM_PROCESSES; ++i) {
-        for (int j = 0; j < NUM_RESOURCES; ++j) {
-            std::cout << max[i][j] << " ";
-        }
-        std::cout << std::endl;
-    }
-
-    std::cout << "Распределение: " << std::endl;
-    for (int i = 0; i < NUM_PROCESSES; ++i) {
-        for (int j = 0; j < NUM_RESOURCES; ++j) {
-            std::cout << allocation[i][j] << " ";
-        }
-        std::cout << std::endl;
-    }
-
-    std::cout << "Потребность: " << std::endl;
-    for (int i = 0; i < NUM_PROCESSES; ++i) {
-        for (int j = 0; j < NUM_RESOURCES; ++j) {
-            std::cout << need[i][j] << " ";
-        }
-        std::cout << std::endl;
-    }
+    // Инициализация всех значений нулями
 }
 
-// Функция для проверки безопасности
-bool is_safe_state() {
-    std::vector<bool> finish(NUM_PROCESSES, false);
-    std::vector<int> work = available;
-    int count = 0;
+void Bankers::input()
+{
+    int i, j;
+    cout << "Введите количество процессов: ";
+    cin >> nop;
+    cout << "Введите количество ресурсов: ";
+    cin >> nor;
+    cout << "Введите выделенные ресурсы для каждого процесса: " << endl;
+    for (i = 0; i < nop; i++)
+    {
+        cout << "\nПроцесс " << i;
+        for (j = 0; j < nor; j++)
+        {
+            cout << "\nРесурс " << j << ": ";
+            cin >> al[i][j];
+        }
+    }
+    cout << "Введите максимальные ресурсы, которые нужны каждому процессу: " << endl;
+    for (i = 0; i < nop; i++)
+    {
+        cout << "\nПроцесс " << i;
+        for (j = 0; j < nor; j++)
+        {
+            cout << "\nРесурс " << j << ": ";
+            cin >> m[i][j];
+            n[i][j] = m[i][j] - al[i][j]; // Вычисление нужных ресурсов
+        }
+    }
+    cout << "Введите текущие доступные ресурсы в системе: ";
+    for (j = 0; j < nor; j++)
+    {
+        cout << "Ресурс " << j << ": ";
+        cin >> avail[j];
+        work[j] = -1;
+    }
+    for (i = 0; i < nop; i++)
+        finish[i] = 0; // Все процессы изначально не завершены
+}
 
-    while (count < NUM_PROCESSES) {
-        bool found = false;
-        for (int i = 0; i < NUM_PROCESSES; ++i) {
-            if (!finish[i]) {
-                bool can_allocate = true;
-                for (int j = 0; j < NUM_RESOURCES; ++j) {
-                    if (need[i][j] > work[j]) {
-                        can_allocate = false;
-                        break;
-                    }
-                }
-
-                if (can_allocate) {
-                    for (int j = 0; j < NUM_RESOURCES; ++j) {
-                        work[j] += allocation[i][j];
-                    }
-                    finish[i] = true;
-                    found = true;
-                    count++;
-                }
+void Bankers::process(int i)
+{
+    unique_lock<mutex> lock(mtx); // Блокировка мьютекса для синхронизации
+    if (finish[i] == 0)
+    {
+        pnum = search(i);
+        if (pnum != -1)
+        {
+            result[k++] = i; // Добавление процесса в результат
+            finish[i] = 1; // Пометка процесса как завершенного
+            for (int j = 0; j < nor; j++)
+            {
+                avail[j] += al[i][j]; // Обновление доступных ресурсов
             }
         }
-
-        if (!found) {
-            return false;
-        }
     }
-
-    return true;
+    lock.unlock(); // Разблокировка мьютекса
 }
 
-// Функция для запроса ресурсов
-void request_resources(int process_id, const std::vector<int>& request) {
-    std::unique_lock<std::mutex> lock(mtx);
+int Bankers::search(int i)
+{
+    for (int j = 0; j < nor; j++)
+        if (n[i][j] > avail[j])
+            return -1; // Если нужные ресурсы превышают доступные, процесс не может быть выполнен
+    return 0;
+}
 
-    bool can_allocate = true;
-    for (int i = 0; i < NUM_RESOURCES; ++i) {
-        if (request[i] > need[process_id][i]) {
-            std::cout << "Процесс " << process_id << " превысило максимально возможное требование." << std::endl;
-            can_allocate = false;
+void Bankers::method()
+{
+    vector<thread> threads;
+    for (int i = 0; i < nop; i++)
+    {
+        threads.emplace_back(&Bankers::process, this, i); // Создание потока для каждого процесса
+    }
+
+    for (auto &t : threads)
+    {
+        t.join(); // Ожидание завершения всех потоков
+    }
+
+    // Проверка, завершены ли все процессы
+    bool flag = true;
+    for (int i = 0; i < nop; i++)
+    {
+        if (finish[i] == 0)
+        {
+            flag = false;
             break;
         }
     }
 
-    if (can_allocate) {
-        for (int i = 0; i < NUM_RESOURCES; ++i) {
-            if (request[i] > available[i]) {
-                std::cout << "Процесс " << process_id << " необходимо подождать, ресурсов не хватает." << std::endl;
-                can_allocate = false;
-                break;
-            }
-        }
+    if (!flag)
+    {
+        cout << "Система не находится в безопасном состоянии, возможен deadlock!" << endl;
     }
-
-    if (can_allocate) {
-        for (int i = 0; i < NUM_RESOURCES; ++i) {
-            available[i] -= request[i];
-            allocation[process_id][i] += request[i];
-            need[process_id][i] -= request[i];
-        }
-
-        if (is_safe_state()) {
-            std::cout << "Процесс " << process_id << " ресурсы выделены." << std::endl;
-        } else {
-            std::cout << "Процесс " << process_id << " необходимо подождать, система не в безопасном состоянии." << std::endl;
-            for (int i = 0; i < NUM_RESOURCES; ++i) {
-                available[i] += request[i];
-                allocation[process_id][i] -= request[i];
-                need[process_id][i] += request[i];
-            }
-        }
+    else
+    {
+        cout << "Система находится в безопасном состоянии, deadlock невозможен!" << endl;
     }
-
-    cv.notify_all();
 }
 
-// Функция для каждого процесса
-void process_function(int process_id) {
-    std::vector<int> request(NUM_RESOURCES, 0);
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_int_distribution<> dis(0, 5);
-
-    for (int i = 0; i < NUM_RESOURCES; ++i) {
-        request[i] = dis(gen);
+void Bankers::display()
+{
+    int i, j;
+    cout << endl << "ВЫВОД:";
+    cout << endl << "========";
+    cout << endl << "ПРОЦЕСС\t     ВЫДЕЛЕННЫЕ\t     МАКСИМАЛЬНЫЕ\tНУЖНЫЕ";
+    for (i = 0; i < nop; i++)
+    {
+        cout << "\nP" << i + 1 << "\t     ";
+        for (j = 0; j < nor; j++)
+        {
+            cout << al[i][j] << "  ";
+        }
+        cout << "\t     ";
+        for (j = 0; j < nor; j++)
+        {
+            cout << m[i][j] << "  ";
+        }
+        cout << "\t     ";
+        for (j = 0; j < nor; j++)
+        {
+            cout << n[i][j] << "  ";
+        }
     }
-
-    request_resources(process_id, request);
+    cout << "\nПоследовательность безопасных процессов: \n";
+    for (i = 0; i < k; i++)
+    {
+        int temp = result[i] + 1;
+        cout << "P" << temp << " ";
+    }
+    cout << "\nПоследовательность небезопасных процессов: \n";
+    int flg = 0;
+    for (i = 0; i < nop; i++)
+    {
+        if (finish[i] == 0)
+        {
+            flg = 1;
+        }
+        cout << "P" << i << " ";
+    }
+    cout << endl << "РЕЗУЛЬТАТ:";
+    cout << endl << "=======";
+    if (flg == 1)
+        cout << endl << "Система не находится в безопасном состоянии, возможен deadlock!";
+    else
+        cout << endl << "Система находится в безопасном состоянии, deadlock невозможен!";
 }
 
-int main() {
-    initialize();
-
-    std::vector<std::thread> threads;
-    for (int i = 0; i < NUM_PROCESSES; ++i) {
-        threads.push_back(std::thread(process_function, i));
-    }
-
-    for (auto& t : threads) {
-        t.join();
-    }
-
-    return 0;
+int main()
+{
+    cout << " АЛГОРИТМ БАНКИРА ДЛЯ ПРЕДОТВРАЩЕНИЯ ДЕДЛОКА " << endl;
+    Bankers B;
+    B.input();
+    B.method();
+    B.display();
+    cout << endl;
 }
